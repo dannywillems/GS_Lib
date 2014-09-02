4cab846992ace2de9baec961a47b6eb1f8df598e
Danny Willems <dan.wil@hotmail.com>
2014-09-02 12:17:49 +0200

    use gs_slist_del function instead of using free inside it

--- c/SLinkedList/srcs/gs_slist_clear.c
+++ c/SLinkedList/srcs/gs_slist_clear.c
@@ -3,14 +3,7 @@
 
 void		gs_slist_clear(t_slist **begin_list)
 {
-	t_slist *tmp;
-
-	tmp = *begin_list;
-	while (tmp)
-	{
-		tmp = (*begin_list)->next;
-		free(*begin_list);
-		*begin_list = tmp;
-	}
+	while (*begin_list)
+		gs_slist_del(begin_list);
 	begin_list = NULL;
 }
