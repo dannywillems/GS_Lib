03404420466c6633f952c21b602a8566e11f1ffa
Danny Willems <dan.wil@hotmail.com>
2014-09-08 14:50:33 +0200

    del exe and change test

--- c/SLinkedList_OO/tests/test_slist.c
+++ c/SLinkedList_OO/tests/test_slist.c
@@ -17,35 +17,25 @@ void	convert(void *data)
 int		main(int argc, char **argv)
 {
 	t_slist *l;
-	t_slist *second;
 	int i;
 
-	second = NULL;
 	l = NULL;
-
 	l = gs_slist_create();
-	second = gs_slist_create();
-	
-	gs_slist_push_front(second, "first elem of second list");
 	gs_slist_push_front(l, argv[0]);
 	for(i = 1;i < argc;i++)
 		gs_slist_push_back(l, argv[i]);
-	
 	gs_slist_push_back(l, "last elem before merging");
-
-	gs_slist_merge(&l, second);
-	gs_slist_clear(&second);
-	
 	gs_slist_print(l, &convert);
+	write(1, "\n", 1);
+	
+	gs_slist_del_at(l, 2);
+	gs_slist_print(l, &convert);
+	write(1, "\n", 1);
+	
+	gs_slist_del_at(l, 1);
+	gs_slist_print(l, &convert);
+	write(1, "\n", 1);
 
-	char **t = gs_slist_toarray(l, 0);
-	l = NULL;
-	while (*t)
-	{
-		write(1, *t, sizeof(*t));
-		t++;
-	}
-
-
+	gs_slist_clear(&l);
 return (0);	
 }
